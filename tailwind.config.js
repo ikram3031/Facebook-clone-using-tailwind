ff3dad0c885b5cb59d4a6f8d2b0f58ff6612dc35
ikram3031 <mdikram.3031@gmail.com>
2022-01-20 22:24:41 +0600

    20/1

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,15 @@
 module.exports = {
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      borderWidth: {
+        DEFAULT: '1px',
+        0: '0px',
+        2: '2px',
+        4: '4px',
+        8: '8px',
+      },
+    },
   },
   plugins: [],
 }
